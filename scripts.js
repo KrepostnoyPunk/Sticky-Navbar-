af777ae4f474ec8e266d644b967f4fd623746548
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2023-11-27 11:47:31 +0300

    almost done, but i want to add some true text

--- scripts.js
+++ scripts.js
@@ -6,6 +6,11 @@ console.log(bottomContainerEl.offsetTop);
 window.addEventListener("scroll", ()=>{
     //console.log('scrolled');
     //console.log(window.scrollY);
+    if(window.scrollY>bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50) {
+        navbarEl.classList.add("active")
+    } else {
+        navbarEl.classList.remove("active")
+    }
 })
 
 
